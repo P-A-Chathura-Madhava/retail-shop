a17f851262800f5bf1bf9fe4ef867cdfbf25e7ee
P-A-CHATHURA-MADHAVA <ctechict101@gmail.com>
2023-05-16 18:45:21 +0530

    Add CustomerService

---
+++ src/main/java/lk/ctech/retailshop/service/CustomerService.java
@@ -0,0 +1,10 @@
+package lk.ctech.retailshop.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class CustomerService {
+}
