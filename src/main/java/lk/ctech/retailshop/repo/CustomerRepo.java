fb4b31fda50bf08005ef661209ba5a75d39f4f14
P-A-CHATHURA-MADHAVA <ctechict101@gmail.com>
2023-05-16 18:44:41 +0530

    Add CustomerRepo

---
+++ src/main/java/lk/ctech/retailshop/repo/CustomerRepo.java
@@ -0,0 +1,7 @@
+package lk.ctech.retailshop.repo;
+
+import lk.ctech.retailshop.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepo extends JpaRepository<Customer, String> {
+}
