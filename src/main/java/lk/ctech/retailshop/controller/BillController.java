59dc2c3aecbe10f74ae37e7a67e625623d4596ff
P-A-CHATHURA-MADHAVA <ctechict101@gmail.com>
2023-05-18 14:25:13 +0530

    Add Bill

--- src/main/java/lk/ctech/retailshop/controller/BillController.java
+++ src/main/java/lk/ctech/retailshop/controller/BillController.java
@@ -1,8 +1,12 @@
 package lk.ctech.retailshop.controller;
 
 import lk.ctech.retailshop.dto.BillDTO;
+import lk.ctech.retailshop.dto.ResponseDTO;
 import lk.ctech.retailshop.service.BillService;
+import lk.ctech.retailshop.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,10 +17,22 @@ import org.springframework.web.bind.annotation.*;
 public class BillController {
     @Autowired
     private BillService billService;
+    @Autowired
+    private ResponseDTO responseDTO;
 
     @PostMapping("/addBill")
-    public BillDTO addBill(@RequestBody BillDTO billDTO){
-        billService.addOrder(billDTO);
-        return billDTO;
+    public ResponseEntity addBill(@RequestBody BillDTO billDTO){
+        try {
+            billService.addOrder(billDTO);
+            responseDTO.setCode(VarList.RSP_SUCCESS);
+            responseDTO.setMessage("Success");
+            responseDTO.setContent(billDTO);
+            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+        }catch (Exception ex){
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage("Error");
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
